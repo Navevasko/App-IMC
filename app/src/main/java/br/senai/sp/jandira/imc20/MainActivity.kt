e8dc53ac9975da4ab98de08bdf56380614edd11c
GUILHERME NAVEVAIKO MOREIRA <21193389@CFP127ALUNO.LOCAL>
2021-09-13 16:44:22 -0300

    Aula dia 13/09. Efetuando cálculo do IMC

--- app/src/main/java/br/senai/sp/jandira/imc20/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc20/MainActivity.kt
@@ -6,19 +6,37 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editPeso = findViewById<EditText>(R.id.edit_peso)
-        val editAltura = findViewById<EditText>(R.id.edit_altura)
-        val textResultado = findViewById<TextView>(R.id.view_resultado)
-        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        //Criando as instancias das views
+        //Para que possam ser manipuladas em kotlin
+        val editTextPeso = findViewById<EditText>(R.id.edit_peso)
+        val editTextAltura = findViewById<EditText>(R.id.edit_altura)
+        val textViewResultado = findViewById<TextView>(R.id.view_resultado)
+        val ButtonCalcular = findViewById<Button>(R.id.button_calcular)
 
-        buttonCalcular.setOnClickListener {
-            Toast.makeText(this, editPeso.text.toString(), Toast.LENGTH_SHORT).show()
+        //Adicionando um evento de click no botão
+        ButtonCalcular.setOnClickListener {
+
+            if (editTextAltura.text.isEmpty()) {
+                editTextAltura.error = "Preencha esse campo"
+            }
+            else if (editTextPeso.text.isEmpty()) {
+                editTextPeso.error = "Preencha esse campo"
+            }
+            else {
+                var peso = editTextPeso.text.toString().toDouble()
+                var altura = editTextAltura.text.toString().toDouble()
+                val imc = peso / (altura * altura)
+
+                textViewResultado.text = String.format("%.1f", imc)
+            }
         }
 
     }
